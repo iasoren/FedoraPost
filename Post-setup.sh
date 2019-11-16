71c5cbbe25cc3fe737ce010ac6d8d1da55263caa
Ian Sorensen <iasoren@Ians-MacBook-Pro.local>
2019-11-16 18:31:09 -0500

    minor fixes

--- Post-setup.sh
+++ Post-setup.sh
@@ -3,25 +3,22 @@
 # Feel free to create new branches if desired.
 # WARNING: This script will be updated ittermitently and may have dead links, may not function, and may completely destroy all your data. I am not responsbile for any problems that may occur. You are using this at your own risk.
 
-# Gnome Tweak Tool and a few customizations
-dnf install -y gnome-tweak-tool
+#COPR Enabling
+dnf copr enable kwizart/fedy
 dnf copr enable snwh/paper
-dnf -y install paper-icon-theme paper-gtk-theme
-#Now using Numix Icons installed through Fedy
 
 #RPM Fusion Repos
 dnf install -y --nogpgcheck http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-
-#Facebook Messenger Repo
-#wget https://bintray.com/aluxian/rpm/rpm -O /etc/yum.repos.d/bintray-aluxian-rpm.repo
 
 #Update the system
 dnf update -y
 dnf install -y dnf-plugin-system-upgrade
 
-#Oh-my-zsh
-#dnf install -y zsh
-#wget --no-check-certificate http://install.ohmyz.sh -O - | sh
+# Gnome Tweak Tool and a few customizations
+dnf install -y gnome-tweak-tool
+dnf -y install paper-icon-theme paper-gtk-theme
+#Now using Numix Icons installed through Fedy
+
 
 #Virtualisation
 dnf install -y @virtualization
@@ -29,43 +26,13 @@ dnf install -y libvirt
 
 #Utilities
 dnf install -y nano
-dnf install -y simple-scan
-dnf config-manager --add-repo=http://negativo17.org/repos/fedora-handbrake.repo
-dnf install -y HandBrake-gui
-dnf install -y HandBrake-cli
-dnf install -y MakeMKV
-dnf install -y libdvdcss
 dnf install -y ffmpeg
 dnf install -y conky
 
-#dnf install -y messengerfordesktop
-
-#dnf install -y eclipse
-#dnf copr enable decathorpe/syncthing -y
-#dnf install -y syncthing
-#dnf install -y syncthing-gtk
-#dnf install -y syncthing-inotify
-
-#Enable Syncthing
-#systemctl --user enable syncthing.service
-#systemctl --user enable syncthing-inotify.service
-#systemctl --user start syncthing.service
-#systemctl --user start syncthing-inotify.service
-
-
-#Games
+#DNF Programs
 dnf install -y steam
-dnf install -y wine
-
-#Internet
 dnf install -y qbittorrent
-
-#Insync
-#rpm --import https://d2t3ff60b2tol4.cloudfront.net/repomd.xml.key
-#dnf config-manager --add-repo http://yum.insynchq.com/fedora/$releasever/
-#dnf install -y insync
-
-#Multimedia
+dnf install -y fedy
 dnf install -y vlc
 dnf install -y kaffeine
 
@@ -88,22 +55,23 @@ rpm -ivh wps*
 
 #Insync Install
 rpm --import https://d2t3ff60b2tol4.cloudfront.net/repomd.xml.key
-cd /
-touch /etc/yum.repos.d/insync.repo
-cd /etc/yum.repos.d/
-echo -e "
-[insync] \n
-name=insync repo \n
-baseurl=http://yum.insync.io/[DISTRIBUTION]/$releasever/ \n
-gpgcheck=1 \n
-gpgkey=https://d2t3ff60b2tol4.cloudfront.net/repomd.xml.key \n
-enabled=1 \n
-metadata_expire=120m \n
-" > insync.repo
-dnf install insync
+dnf config-manager --add-repo http://yum.insynchq.com/fedora/$releasever/
+#cd /
+#touch /etc/yum.repos.d/insync.repo
+#cd /etc/yum.repos.d/
+#echo -e "
+#[insync] \n
+#name=insync repo \n
+#baseurl=http://yum.insync.io/[DISTRIBUTION]/$releasever/ \n
+#gpgcheck=1 \n
+#gpgkey=https://d2t3ff60b2tol4.cloudfront.net/repomd.xml.key \n
+#enabled=1 \n
+#metadata_expire=120m \n
+#" > insync.repo
+dnf install -y insync
 
 #Remove undesired packages
-#dnf remove -y libreoffice*
+dnf remove -y libreoffice*
 
 
 #This is a list of extensions to Gnome that I have installed/enabled
