95122aacfeca7e2ce57fbfbb74ff12bef99fa4ab
Ian Sorensen <iasoren@Ians-MacBook-Pro.local>
2019-11-16 22:17:22 -0500

    Remove conky, remove themes to install in Fedy

--- Post-setup.sh
+++ Post-setup.sh
@@ -5,7 +5,6 @@
 
 #COPR Enabling
 dnf copr enable -y kwizart/fedy
-dnf copr enable -y snwh/paper
 
 #RPM Fusion Repos
 dnf install -y --nogpgcheck http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
@@ -16,7 +15,6 @@ dnf install -y dnf-plugin-system-upgrade
 
 # Gnome Tweak Tool and a few customizations
 dnf install -y gnome-tweak-tool
-dnf -y install paper-icon-theme paper-gtk-theme
 #Now using Numix Icons installed through Fedy
 
 
@@ -27,7 +25,6 @@ dnf install -y libvirt
 #Utilities
 dnf install -y nano
 dnf install -y ffmpeg
-dnf install -y conky
 
 #DNF Programs
 dnf install -y steam
@@ -70,55 +67,6 @@ dnf install -y insync
 #Remove undesired packages
 dnf remove -y libreoffice*
 
-echo "Conky widget by m@, based on Manjaro Openbox edition conky" >> .conkyrc
-echo "update_interval 1" >> .conkyrc
-echo "total_run_times 0" >> .conkyrc
-echo "net_avg_samples 1" >> .conkyrc
-echo "cpu_avg_samples 1" >> .conkyrc
-echo "imlib_cache_size 0" >> .conkyrc
-echo "double_buffer yes" >> .conkyrc
-echo "no_buffers yes" >> .conkyrc
-echo "use_xft yes" >> .conkyrc
-echo "xftfont Droid Sans:size=9" >> .conkyrc
-echo "override_utf8_locale yes" >> .conkyrc
-echo "text_buffer_size 2048" >> .conkyrc
-echo "own_window yes" >> .conkyrc
-echo "own_window_type desktop" >> .conkyrc
-echo "own_window_transparent yes" >> .conkyrc
-echo "own_window_hints undecorated,sticky,skip_taskbar,skip_pager" >> .conkyrc
-echo "alignment top_right" >> .conkyrc
-echo "gap_x 10" >> .conkyrc
-echo "gap_y 60" >> .conkyrc
-echo "minimum_size 280 0" >> .conkyrc
-echo "default_bar_size 30 8" >> .conkyrc
-echo "draw_shades no" >> .conkyrc
-echo "default_color c5c5c5" >> .conkyrc
-echo "default_shade_color 000000" >> .conkyrc
-echo "color0 8DBC68" >> .conkyrc
-echo "color1 c5c5c5" >> .conkyrc
-echo "color2 c5c5c5" >> .conkyrc
-echo "own_window_argb_visual yes" >> .conkyrc
-echo "own_window_colour 000000" >> .conkyrc
-echo "own_window_argb_value 0" >> .conkyrc
-
-echo "TEXT" >> .conkyrc
-echo "${goto 180}${color0}${font Droid Sans:style=bold:size=13}${voffset 35}${exec whoami}" >> .conkyrc
-echo "${voffset -30}${font Droid Sans:style=Bold:size=12}${color}${goto 40}${uptime}" >> .conkyrc
-echo "${goto 80}${color1}${font Droid Sans:style=Bold:size=12}${color1}${sysname}${color}${font}" >> .conkyrc
-echo "${voffset 40}${font Droid Sans:style=Bold:size=11}${goto 128}CPU0${goto 218}GHz" >> .conkyrc
-echo "${font Droid Sans:style=Bold:size=11}${goto 128}${cpu cpu1}%${goto 218}${freq_g}" >> .conkyrc
-echo "${voffset 40}${font Droid Sans:style=Bold:size=11}${goto 88}CPU1${goto 175}${font Droid Sans:style=Bold:size=11}CPU2" >> .conkyrc
-echo "${font Droid Sans:style=Bold:size=11}${goto 88}${cpu cpu2}%${goto 175}${font Droid Sans:style=Bold:size=11}${color1}${cpu cpu3}%" >> .conkyrc
-echo "${voffset 40}${goto 45}T°C${color1}${goto 128}CPU3" >> .conkyrc
-echo "${goto 45}${font Droid Sans:style=Bold:size=10}${exec sensors | grep 'Package id 0' | cut -c17-20}°C${goto 134}${font Droid Sans:style=Bold:size=11}${cpu cpu4}%${font Droid Sans:style=Bold:size=11}${color1}${font}" >> .conkyrc
-echo "${font Droid Sans:style=Bold:size=11}${font}" >> .conkyrc
-echo "${voffset 10}${goto 90}${font Droid Sans:style=Bold:size=11}RAM" >> .conkyrc
-echo "${goto 90}${font Droid Sans:style=Bold:size=11}${memperc}%" >> .conkyrc
-echo "${image /usr/share/conkypic/lsd.png -p 10,0}" >> .conkyrc
-
-echo "Please copy the conky image to /usr/share/conkypic/lsd.png"
-echo "Please enable in Gnome..."
-echo " Application menu, Background logo, Caffeine, Dash to dock, transparent top bar, and topicons plus"
 
 #This is a list of extensions to Gnome that I have installed/enabled
 # Applications Menu
